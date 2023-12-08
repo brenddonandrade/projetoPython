b1b311952633af16c5f7c18c781d399886ac517e
Brenddon Andrade <brenddonandrade@gmail.com>
2023-12-08 16:26:36 -0300

    Terminado level 1

--- pacman.py
+++ pacman.py
@@ -26,14 +26,26 @@ class Pacman(object):
         #self.direction = direction
         #self.node = self.getNewTarget(direction)
         #self.setPosition()
+
         if self.overshotTarget():
             self.node = self.target
             self.target = self.getNewTarget(direction)
             if self.target is not self.node:
                 self.direction = direction
+            #else:
+             #   self.direction = STOP
+            #self.setPosition()
             else:
+                self.target = self.getNewTarget(self.direction)
+
+            if self.target is self.node:
                 self.direction = STOP
             self.setPosition()
+
+        else:
+            if self.oppositeDirection(direction):
+                self.reverseDirection()
+    
 
 
     def validDirection(self, direction):
@@ -72,4 +84,17 @@ class Pacman(object):
             node2Target = vec1.magnitudeSquared()
             node2Self = vec2.magnitudeSquared()
             return node2Self >= node2Target
+        return False
+
+
+    def reverseDirection(self):
+        self.direction *= -1
+        temp = self.node
+        self.node = self.target
+        self.target = temp
+
+    def oppositeDirection(self, direction):
+        if direction is not STOP:
+            if direction == self.direction * -1:
+                return True
         return False
